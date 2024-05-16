d8551319529a3319576a3bb76e1c8aae1a23e8d7
Dariusz Legizynski <d.legizynski@.com>
2024-05-16 23:17:57 +0200

    fix card height

--- src/components/Event/EventsListCard.tsx
+++ src/components/Event/EventsListCard.tsx
@@ -10,7 +10,7 @@ const EventListCard = ({ event }: { event: Event }) => {
 		<li key={event.id} className="py-8">
 			<Link to={`/events/${event.id}`} className="grid gap-y-2 bg-secondary rounded-lg">
 				<img className="rounded-t-lg sm:min-h-80" src={`${JSON_URL}/${event.imagePath}`} alt={event.imgAlt} width={400} height={300} />
-				<section className="px-4 pb-4 sm:min-h-48">
+				<section className="px-4 pb-4 sm:min-h-52">
 					<p className="text-end text-grey">{event.eventType}</p>
 					<div className="h3 font-bold !text-primary text-balance pt-2 pb-4 min-h-20">{event.title}</div>
 					<div className="flex justify-between pb-2 text-grey">
